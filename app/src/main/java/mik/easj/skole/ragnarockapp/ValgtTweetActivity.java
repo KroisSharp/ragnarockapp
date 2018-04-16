12235245d87693c1e7bac17340c33990bb3451a0
kroissharp <mikx0067@edu.easj.dk>
2018-04-16 13:48:38 +0200

    lavet acitivtys

---
+++ app/src/main/java/mik/easj/skole/ragnarockapp/ValgtTweetActivity.java
@@ -0,0 +1,13 @@
+package mik.easj.skole.ragnarockapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ValgtTweetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_valgt_tweet);
+    }
+}
